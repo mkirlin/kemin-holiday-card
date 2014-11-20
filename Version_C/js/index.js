558e76d5023ca569943540ac8279715e5d7edaf2
Michael Kirlin <mkirlin@gmail.com>
2014-11-19 20:26:26 -0600

    Adding latest version, trying to fix resizing issues

--- Version_C/js/index.js
+++ Version_C/js/index.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-	window.onresize = function(){ location.reload(); }
+	// window.onresize = function(){ location.reload(); }
 
-	var holiday_music = document.getElementById("holiday_music");
-	holiday_music.autoplay = true;
-	holiday_music.loop = true;
+	// var holiday_music = document.getElementById("holiday_music");
+	// holiday_music.autoplay = true;
+	// holiday_music.loop = true;
 	
 	$(".holiday_image").hide();
 	$(".holiday_message").hide();
@@ -29,3 +29,10 @@ function animateImages(img) {
 		animateImages($(img).next(".holiday_image"));
 	})
 };
+
+// All items on page have name ids
+// Load into an array when they arrive on the page
+// Pick items from the array at random, fade them in
+// If array is empty, fade in final logo
+// If everything is loaded, wait a beat, then fade out everything and show the message
+// If you've shown the message, wait a few seconds and then show the logo
---
+++ Version_D/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	// All items on page have name ids
+	// Load into an array when they arrive on the page
+	// Pick items from the array at random, fade them in
+	// If array is empty, fade in final logo
+	// If everything is loaded, wait a beat, then fade out everything and show the message
+	// If you've shown the message, wait a few seconds and then show the logo
+
+	$(".holiday_message").hide();
+	$(".logo").hide();
+});
