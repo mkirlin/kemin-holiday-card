929a45b2bbe48de0e86906295726d24dac53de71
Michael Kirlin <mkirlin@gmail.com>
2014-12-02 10:05:39 -0600

    Finally found an audio library that did what I wanted it to do.

--- Version_E/index.js
+++ Version_E/index.js
@@ -1,8 +1,4 @@
 $(document).ready(function() {
-	// Play music on page load
-	lowLag.init({"urlPrefix":"assets/", "audioTagTimeToLive":65000});
-	lowLag.load(["holiday_music.mp3","holiday_music.ogg"],"holiday_music");
-	lowLag.play("holiday_music");
 
 	// All items on page are hidden
 	$(".holiday_image").css("opacity", 0)
@@ -15,9 +11,14 @@ $(document).ready(function() {
 		imageArray.push($(this).attr('id'))
 	});
 
-	// Delay images until after music starts
-	var delayedStart = setInterval(function() {fadeInImages()}, 1800);
-
+	// Play music on page load
+	holiday_music = new Howl({
+		urls: ["assets/holiday_music.mp3", "assets/holiday_music.ogg"],
+		autoplay:true,
+		onplay: function() {
+			var delayStart = setInterval(function() {fadeInImages()}, 1500);
+		}
+	});
 
 });
 
@@ -28,13 +29,12 @@ function fadeInImages() {
 		var randomIndex = Math.floor((Math.random() * imageArray.length));
 		var imageToFadeIn = imageArray[randomIndex];
 		imageArray.splice(randomIndex, 1);
-		$("#"+imageToFadeIn).fadeTo("slow", 1, function() {
+		$("#"+imageToFadeIn).fadeTo(800, 1, function() {
 			fadeInImages();
 		});
 	} else {
 		$("#small_logo").fadeIn(600, function() {
 			$(".block").delay(2000).fadeOut(300, function() {
-				// If everything is loaded, wait a beat, then fade out everything and show the message
 				fadeInMessageAndLogo();
 			});
 		});
@@ -42,14 +42,15 @@ function fadeInImages() {
 };
 
 function fadeInMessageAndLogo() {
-	// Once you've shown the message, wait a few seconds and then show the logo
-	$(".holiday_message").fadeIn(300).delay(5000).fadeOut(300, function() {
+	$(".holiday_message").fadeIn(800).delay(6000).fadeOut(800, function() {
 		$(".holiday_message").remove();
 		$("#small_logo").remove();
-		$(".big_logo").fadeIn(300, function() {
-			setInterval(function() {
-				$('#lowLag').animate({volume: 0.0}, 6000)
-			}, 5000);
+		$(".big_logo").fadeIn(800, function() {
+			var delayFinish = setInterval(function() {
+				$(".big_logo").fadeOut(1000, function() {
+					holiday_music.fadeOut(0.0, 1500);
+				});
+			}, 6000);
 		});
 	});
 };
